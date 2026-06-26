0bb93a392101dc24de5a7ab1940b06b3cb52bb00
imene <imane.bendjemaa@ensia.edu.dz>
2026-06-26 18:55:58 +0100

    complete task1.2

--- main.dart
+++ main.dart
@@ -1,4 +1,8 @@
 void main(){
+  //task1
+
+  //task1.1
+
   const appName = "StudentTracker";
   final studentFullName = "imane Bendjemaa";
   var studentGpa = 333;
@@ -12,5 +16,17 @@ void main(){
   GPA: [$studentGpa]
   Scholarship: [${studentScholar ?? "none"}]
   ''');
+
+  //task1.2
+  String rawScore = "87";
+  String rawAverage = "14.75";
+
+  print(int.parse(rawScore) + 13);
+  double av = double.parse(rawAverage) * 2;
+  print(av);
+
+  print(av.toString().length);
   
+  
+
 }
